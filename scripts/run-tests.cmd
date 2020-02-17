38a6ff25fd85845f04d64b62e0734e0b3541407c
poveden <jorge.poveda@gmail.com>
2020-02-17 23:51:09 +0100

    Exit batch file on build/test errors

--- scripts/run-tests.cmd
+++ scripts/run-tests.cmd
@@ -3,6 +3,7 @@
 pushd %~dp0..
 
 dotnet test -c Release -s Common.runsettings
+if %errorlevel% neq 0 exit /b %errorlevel%
 dotnet tool restore
 dotnet reportgenerator --reports:**\coverage\cobertura.xml --targetdir:coverage\report
 
